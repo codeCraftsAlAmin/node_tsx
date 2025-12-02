633022ca80aa9b72eea991dfb72e860fb58f8c72
Code Crafts <codecraftsman12@gmail.com>
2025-12-02 19:00:32 +0600

    changes

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from "express";
 import { Pool } from "pg";
 import dotenv from "dotenv";
 import path from "path";
+import { title } from "process";
 
 dotenv.config({ path: path.join(process.cwd(), ".env") });
 
@@ -46,10 +47,241 @@ const initDb = async () => {
 
 initDb();
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("This is your server");
+// * users route
+// create user
+app.post("/users", async (req: Request, res: Response) => {
+  // console.log(req.body);
+  const { name, email, age } = req.body;
+
+  try {
+    const result = await pool.query(
+      `INSERT INTO users(name, email, age) VALUES($1, $2, $3)  RETURNING *`,
+      [name, email, age]
+    );
+
+    // console.log(result.rows[0]);
+    res.status(201).send({
+      success: true,
+      message: "Data has been created",
+      result: result.rows[0],
+    });
+  } catch (err: any) {
+    // console.log(error.message);
+    res.status(501).send({
+      success: false,
+      message: err.message,
+    });
+  }
 });
 
+// get all users
+app.get("/users", async (req: Request, res: Response) => {
+  try {
+    const result = await pool.query(`SELECT * from users`);
+    res.status(200).send({
+      success: false,
+      message: "Data retrived successfully",
+      data: result.rows,
+    });
+  } catch (err: any) {
+    res.status(500).send({
+      success: false,
+      message: err.message,
+    });
+  }
+});
+
+// get single user
+app.get("/users/:id", async (req: Request, res: Response) => {
+  try {
+    // console.log(req.params.id)
+    const result = await pool.query(`SELECT * FROM users WHERE id = $1`, [
+      req.params.id,
+    ]);
+
+    if (result.rows.length <= 0) {
+      res.status(500).send({
+        success: false,
+        message: "User not found",
+      });
+    } else {
+      res.status(200).send({
+        success: false,
+        message: "User retrived successfully",
+        data: result.rows[0],
+      });
+    }
+
+    console.log(result.rows);
+  } catch (err: any) {
+    res.status(500).send({
+      success: false,
+      message: err.message,
+    });
+  }
+});
+
+// update user
+app.put("/users/:id", async (req: Request, res: Response) => {
+  try {
+    const { name, email } = req.body;
+    const result = await pool.query(
+      `UPDATE users SET name=$1, email=$2 WHERE id=$3 RETURNING *`,
+      [name, email, req.params.id]
+    );
+
+    res.status(200).send({
+      success: true,
+      message: "User updated successfully",
+      data: result.rows[0],
+    });
+  } catch (err: any) {
+    res.status(501).send({
+      success: false,
+      message: err.message,
+    });
+  }
+});
+
+// delete user
+app.delete("/users/:id", async (req: Request, res: Response) => {
+  try {
+    await pool.query(`DELETE FROM users WHERE id=$1`, [req.params.id]);
+
+    res.status(200).send({
+      success: true,
+      message: "User updated successfully",
+      data: null,
+    });
+  } catch (err: any) {
+    res.status(501).send({
+      success: false,
+      message: err.message,
+    });
+  }
+});
+
+
+// * todos router
+// create todos
+app.post("/todos", async (req: Request, res: Response) => {
+  try {
+    const { user_id, title } = req.body;
+    const result = await pool.query(
+      `INSERT INTO todos(user_id, title) VALUES($1, $2)  RETURNING *`,
+      [user_id, title]
+    );
+
+    res.status(201).send({
+      success: true,
+      message: "todos has been created",
+      data: result.rows[0],
+    });
+  } catch (err: any) {
+    res.status(501).send({
+      success: false,
+      message: err.message,
+    });
+  }
+});
+
+// get all users
+app.get("/todos", async (req: Request, res: Response) => {
+  try {
+    const result = await pool.query(`SELECT * from todos`);
+    res.status(200).send({
+      success: false,
+      message: "Data retrived successfully",
+      data: result.rows,
+    });
+  } catch (err: any) {
+    res.status(500).send({
+      success: false,
+      message: err.message,
+    });
+  }
+});
+
+// get single user
+app.get("/todos/:id", async (req: Request, res: Response) => {
+  try {
+    // console.log(req.params.id)
+    const result = await pool.query(`SELECT * FROM todos WHERE id = $1`, [
+      req.params.id,
+    ]);
+
+    if (result.rows.length <= 0) {
+      res.status(500).send({
+        success: false,
+        message: "Data not found",
+      });
+    } else {
+      res.status(200).send({
+        success: false,
+        message: "Data retrived successfully",
+        data: result.rows[0],
+      });
+    }
+
+    console.log(result.rows);
+  } catch (err: any) {
+    res.status(500).send({
+      success: false,
+      message: err.message,
+    });
+  }
+});
+
+// update todo
+app.put("/todos/:id", async (req: Request, res: Response) => {
+  try {
+    const { title } = req.body;
+    const result = await pool.query(
+      `UPDATE todos SET title=$1 WHERE id=$2 RETURNING *`,
+      [title, req.params.id]
+    );
+
+    res.status(200).send({
+      success: true,
+      message: "User updated successfully",
+      data: result.rows[0],
+    });
+  } catch (err: any) {
+    res.status(501).send({
+      success: false,
+      message: err.message,
+    });
+  }
+});
+
+// delete todo
+app.delete("/todos/:id", async (req: Request, res: Response) => {
+  try {
+    await pool.query(`DELETE FROM todos WHERE id=$1`, [req.params.id]);
+
+    res.status(200).send({
+      success: true,
+      message: "Data deleted successfully",
+      data: null,
+    });
+  } catch (err: any) {
+    res.status(501).send({
+      success: false,
+      message: err.message,
+    });
+  }
+});
+
+// * route not found error
+app.use((req: Request, res: Response) => {
+  res.status(404).send({
+    success: false,
+    message: "Route not found",
+    path: req.path,
+  });
+});
+
+// * localhost runner
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
